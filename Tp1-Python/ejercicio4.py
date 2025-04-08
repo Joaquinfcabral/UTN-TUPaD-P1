94ddea25f45a3be315fdb7c5c6d079c82f01d52d
Joaquin Fernandez Cabral <93291749+Joaquinfcabral@users.noreply.github.com>
2025-04-08 20:45:54 -0300

    Add files via upload

---
+++ Tp1-Python/ejercicio4.py
@@ -0,0 +1,13 @@
+radio = int(input("Por favor ingrese el radio de su círculo: "))
+
+
+pi = float(3.14)
+
+area = pi * (radio ** 2)
+
+
+perimetro = 2 * pi * radio
+
+
+print(f"El area de su circulo es {area} y el perimetro es {perimetro}")
+
