4a453881a1f80ce1fb9efb844a3e625e34a21b7d
Joaquin Fernandez Cabral <93291749+Joaquinfcabral@users.noreply.github.com>
2025-04-08 20:48:04 -0300

    Subida de mi primer TP.

---
+++ 01 Estructuras Secuenciales/ejercicio8.py
@@ -0,0 +1,9 @@
+
+peso = float(input("Ingresa tu peso en kilogramos (kg): "))
+altura = float(input("Ingresa tu altura en metros (m): "))
+
+
+imc = peso / (altura ** 2)
+
+
+print(f"Tu índice de masa corporal (IMC) es: {imc}")
